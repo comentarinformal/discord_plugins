878dcd7d6cdede3cb988612f84c67d1f4d12122e
Comentarinformal <coment@comentarinform.al>
2018-07-10 14:30:05 +0200

    Update Rot13.plugin.js
    Changed autotranslating to avoid spoiling, which is the point of this in the first place. Now you click on the [ROT13]xxxxxx text, or hover over it.

--- Rot13.plugin.js
+++ Rot13.plugin.js
@@ -12,9 +12,9 @@
 
 var Rot13 = function() {};
 
-Rot13.prototype.getName = function() {return "Rot13 Thingamagicky";};
-Rot13.prototype.getDescription = function() {return "You probably guess what it does. /rot13 or /rot <text>. Hold CTRL to avoid auto-sending the message, for 'decryption'. ";};
-Rot13.prototype.getVersion = function() {return "1.9";};
+Rot13.prototype.getName = function() {return "Rot13 Modifier";};
+Rot13.prototype.getDescription = function() {return "You probably guess what it does. /rot13 or /rot <text> to generate the tags. Hover over a rot13-tagged text, or click on it, to translate it. ";};
+Rot13.prototype.getVersion = function() {return "2.1";};
 Rot13.prototype.getAuthor = function() {return "Coment";};
 Rot13.prototype.getRawURL = function() {return 'https://raw.githubusercontent.com/comentarinformal/discord_plugins/master/Rot13.plugin.js';};
 
@@ -39,8 +39,15 @@ Rot13.prototype.handleChat = function(){
 		
 		$(".message-text .markup").each(function(z,elem) {
 			var msgVal = elem.innerText;
-			if(msgVal.startsWith('[rot13]')){
-				$(elem).html("[ROT13] <i><b>"+rotate(msgVal.slice(7))+"</b></i>")
+			//console.log(msgVal);
+			if(msgVal.indexOf('[rot13]') > -1){
+				var encryptedText = msgVal.slice(msgVal.indexOf('[rot13]')+7)
+				var rotatedText = rotate(encryptedText);
+				if(false){ ////If I ever do a Settings to choose wether to auto-translate or not, it will be here
+					$(elem).html(msgVal.slice(0,msgVal.indexOf('[rot13]'))+"<span title='"+rotatedText+"'> [ROT13] <i><b>"+rotatedText+"</b></i></span>")
+				}else {
+					$(elem).html(msgVal.slice(0,msgVal.indexOf('[rot13]'))+"<span title='[ROT13]"+rotatedText+"' onclick='Rot13.switch(this)'><i><b>[ROT13]"+encryptedText+"</b></i></span>")
+				}
 			}
 		});
 		
@@ -76,6 +83,12 @@ Rot13.prototype.unload = function() {this.stop();}
 
 Rot13.prototype.attachHandler = function() {document.addEventListener("keydown", this.handleKeypress, false);}
 function rotate(str){return str.replace(/[a-zA-Z]/g,function(c){return String.fromCharCode((c<="Z"?90:122)>=(c=c.charCodeAt(0)+13)?c:c-26);});}
-
+Rot13.switch = function(elem){
+	var text1 = elem.innerText;
+	var text2 = elem.getAttribute('title');
+	elem.innerHTML = "<i><b>"+text2+"</b></i>";
+	elem.setAttribute('title',text1);
+	return true;
+}
 //TOTALLY not stolen from https://stackoverflow.com/questions/617647/where-is-my-one-line-implementation-of-rot13-in-javascript-going-wrong
 // and https://stackoverflow.com/questions/23892547/what-is-the-best-way-to-trigger-onchange-event-in-react-js 
