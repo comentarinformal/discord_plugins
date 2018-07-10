7e02176a0744b773d3e7a6efdc17bd06aaae8ee1
Comentarinformal <coment@comentarinform.al>
2018-07-10 03:51:45 +0200

    Update Rot13.plugin.js
    Adding auto-parsing, messages are prefixed to be handled now ("[rot13]code")

--- Rot13.plugin.js
+++ Rot13.plugin.js
@@ -14,16 +14,36 @@ var Rot13 = function() {};
 
 Rot13.prototype.getName = function() {return "Rot13 Thingamagicky";};
 Rot13.prototype.getDescription = function() {return "You probably guess what it does. /rot13 or /rot <text>. Hold CTRL to avoid auto-sending the message, for 'decryption'. ";};
-Rot13.prototype.getVersion = function() {return "1.4";};
+Rot13.prototype.getVersion = function() {return "1.8";};
 Rot13.prototype.getAuthor = function() {return "Coment";};
-Rot13.prototype.getSettingsPanel = function() {};
 
 Rot13.prototype.load = function() {this.attachHandler();};
-Rot13.prototype.start = function() {this.attachHandler();};
-Rot13.prototype.onSwitch = this.start;
+Rot13.prototype.start = function() {this.attachHandler();this.handleChat()};
+Rot13.prototype.onSwitch = function(){/*this.start();*/}
+
+
+Rot13.prototype.observer = function (e) {
+	if(e.addedNodes.length > 0 && typeof e.addedNodes[0].className !== undefined && e.addedNodes[0].className.indexOf('message') > -1){
+		this.handleChat();
+	}
+}
+
+Rot13.prototype.handleChat = function(){
+	setTimeout(function() {
+		
+		$(".message-text .markup").each(function(z,elem) {
+			var msgVal = elem.innerText;
+			//console.log(msgVal);
+			if(msgVal.startsWith('[rot13]')){
+				$(elem).html("[ROT13] <i><b>"+rotate(msgVal.slice(7))+"</b></i>")
+			}
+		});
+		
+	},250);
+}
 
 Rot13.prototype.handleKeypress = function(e){
-	var textAreaField,keycode, command, text, rotatedText;
+	var textAreaField,keycode, command, text;
 	
 	textAreaField = $("textarea")[0];
 	if(textAreaField != document.activeElement) return;
@@ -35,23 +55,22 @@ Rot13.prototype.handleKeypress = function(e){
 	if(!command.toLowerCase().startsWith('/rot ') && !command.toLowerCase().startsWith('/rot13 ')) return;
 	command.toLowerCase().startsWith('/rot13 ') ? text = command.slice(7) : text = command.slice(5);
 	
-	rotatedText = text.replace(/[a-zA-Z]/g,function(c){return String.fromCharCode((c<="Z"?90:122)>=(c=c.charCodeAt(0)+13)?c:c-26);});
-	
 	var nativeInputValueSetter = Object.getOwnPropertyDescriptor(window.HTMLTextAreaElement.prototype, "value").set;
-	nativeInputValueSetter.call(textAreaField, rotatedText);
+	nativeInputValueSetter.call(textAreaField, "[rot13]"+rotate(text));
 	
 	var ev2 = new Event('input', { bubbles: true});
 	textAreaField.dispatchEvent(ev2);
-	if(e.ctrlKey === true){
+	//if(e.ctrlKey === true){ yknowwhat, let's make that default. 
 		e.preventDefault();
 		e.stopPropagation();
-	}
+	//}
 }
 
 Rot13.prototype.stop = function() {document.removeEventListener("keydown", this.handleKeypress, false);};
-Rot13.prototype.unload = this.stop;
+Rot13.prototype.unload = function() {this.stop();}
 
 Rot13.prototype.attachHandler = function() {document.addEventListener("keydown", this.handleKeypress, false);}
+function rotate(str){return str.replace(/[a-zA-Z]/g,function(c){return String.fromCharCode((c<="Z"?90:122)>=(c=c.charCodeAt(0)+13)?c:c-26);});}
 
 //TOTALLY not stolen from https://stackoverflow.com/questions/617647/where-is-my-one-line-implementation-of-rot13-in-javascript-going-wrong
 // and https://stackoverflow.com/questions/23892547/what-is-the-best-way-to-trigger-onchange-event-in-react-js 
